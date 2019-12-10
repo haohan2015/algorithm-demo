230be31cc6919ea843b305e91157d294e7a82339
lipeng3@01zhuanche.com <lipeng3@01zhuanche.com>
2019-12-10 19:17:13 +0800

    各位相加

---
+++ algorithm-demo-core/src/main/java/com/algorithm/MembersSum.java
@@ -0,0 +1,47 @@
+package com.algorithm;
+
+/**
+ * @author admin
+ * @Description: 各位相加
+ * @date 2019/12/10 19:08
+ *
+ * 给定一个非负整数 num，反复将各个位上的数字相加，直到结果为一位数。
+
+示例:
+
+输入: 38
+输出: 2
+解释: 各位相加的过程为：3 + 8 = 11, 1 + 1 = 2。 由于 2 是一位数，所以返回 2。
+进阶:
+你可以不使用循环或者递归，且在 O(1) 时间复杂度内解决这个问题吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/add-digits
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MembersSum {
+
+    public static int addDigits(int num) {
+        if(num < 10){
+            return num;
+        }
+
+        while (true){
+            int newNum = 0;
+            while (num >= 10){
+                newNum += num % 10;
+                num /= 10;
+            }
+            newNum += num;
+            if(newNum < 10){
+                return newNum;
+            }
+            num = newNum;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("args = [" + addDigits(10) + "]");
+    }
+
+}
